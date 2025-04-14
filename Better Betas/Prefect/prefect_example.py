b15d17c76b8795e102b741702dbe99bf0e2b1d29
Wojtek_ftw <wojtekzacherek@gmail.com>
2025-04-14 00:44:12 -0500

    Update Better Betas
    * Populate streamlit guidelines and experiences
    * Very simple strreamlit app
    * Remove Pydantic and Prefect

--- Better Betas/Prefect/prefect_example.py
+++
@@ -1,9 +0,0 @@
-from prefect import flow, task
-
-@task
-def clean_data(data):
-    return [d for d in data if d is not None]
-
-@flow
-def pipeline():
-    return clean_data([1, None, 3])
--- Better Betas/Pydantic/pydantic_example.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    count: int
-
-item = Item(name="apple", count="3")  # auto-casts str to int!
